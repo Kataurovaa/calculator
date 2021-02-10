7680b143bb01ba8780bea517b1002dbe6457b0dd
Kataurovaa <56738175+Kataurovaa@users.noreply.github.com>
2021-02-10 17:59:23 +0300

    Не вышло
    Попытка создать анимационный фон(

---
+++ media/js/main.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function(){
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', 'media/js/assets/particles/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
+});
