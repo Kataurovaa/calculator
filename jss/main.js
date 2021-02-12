fb8ef5a48df49a7005ce69cbd8d47fa6c886139a
Kataurovaa <56738175+Kataurovaa@users.noreply.github.com>
2021-02-13 00:04:54 +0300

    upload
    Окно загрузить

---
+++ jss/main.js
@@ -0,0 +1,17 @@
+function viewReg(){ 
+    if (document.getElementById("registration").style.visibility == 'visible') { 
+    document.getElementById("registration").style.visibility = "hidden";
+    }
+    else {
+    document.getElementById("registration").style.visibility = "visible";
+    }
+    };
+
+function viewFile(){ 
+    if (document.getElementById("upload").style.visibility == 'visible') { 
+    document.getElementById("upload").style.visibility = "hidden";
+    }
+    else {
+    document.getElementById("upload").style.visibility = "visible";
+    }
+    };
