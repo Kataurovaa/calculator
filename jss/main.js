72c1fed724390c50bbbb9c0792ae293efc5f710f
Kataurovaa <56738175+Kataurovaa@users.noreply.github.com>
2021-02-13 03:33:01 +0300

    всплывающие окна
    Сделано:
    - затемнение фона при открытии регистрации и поля выбора файла
    - кнопки для выхода
    - скорректировано положение поля для выбора файла
    
    Улучшить:
    - дизайн поля для выбора файла
    - расположение кнопок выхода
    - надпись "войти и зарегистрироваться" уменьшилась

--- jss/main.js
+++ jss/main.js
@@ -1,17 +1,15 @@
 function viewReg(){ 
-    if (document.getElementById("registration").style.visibility == 'visible') { 
-    document.getElementById("registration").style.visibility = "hidden";
-    }
-    else {
     document.getElementById("registration").style.visibility = "visible";
-    }
+    document.getElementById("fill-black").style.visibility = "visible";
     };
 
 function viewFile(){ 
-    if (document.getElementById("upload").style.visibility == 'visible') { 
-    document.getElementById("upload").style.visibility = "hidden";
-    }
-    else {
     document.getElementById("upload").style.visibility = "visible";
-    }
+    document.getElementById("fill-black").style.visibility = "visible";
     };
+
+function closeFill(){
+    document.getElementById("fill-black").style.visibility = "hidden";
+    document.getElementById("registration").style.visibility = "hidden";
+    document.getElementById("upload").style.visibility = "hidden";
+}
