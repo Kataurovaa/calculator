41ddc64010368dedfda2dd5b0139a9853c30752d
Timur Kornilov <timadzr@gmail.com>
2021-03-09 00:11:40 +0300

    Final

---
+++ calc/cmake-build-debug/CMakeFiles/calc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc.dir/main.c.obj"
+  "calc.exe"
+  "calc.exe.manifest"
+  "calc.pdb"
+  "libcalc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- index.php
+++ index.php
@@ -111,15 +111,15 @@
 			<div class="d-field">
 				<h1 class="d-word">
 				<?php
-					exec("C:/Server/data/htdocs/calculator/calc/release/calc.exe") or die("Err");
-					$fd = fopen("calc/Release/out.txt", 'r') or die("не удалось открыть файл");
+					exec("C:/Server/data/htdocs/calculator/calc/cmake-build-debug/calc.exe") or die("Err");
+					$fd = fopen("calc/cmake-build-debug/out.txt", 'r') or die("не удалось открыть файл");
 					while(!feof($fd))
 					{
     					$str = fgets($fd);
     					echo $str;
     					echo "<br/>";
 					}
-					fclose("calc/Release/out.txt");				
+					fclose("calc/cmake-build-debug/out.txt");				
 				?>
 				</h1>
 			</div>
